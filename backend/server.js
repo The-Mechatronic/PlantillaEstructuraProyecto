382bdfaecaeefa39750facc6a714e9b561b4a857
Edwar Hernando Velásquez <ricoprogramar@gmail.com>
2025-04-01 15:01:36 -0500

    Registro de usuario falta configurar CORS
    Registro de usuario falta configurar CORS

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,30 @@ const app = express();
 
 // 📌 Middlewares
 app.use(express.json()); // Habilita el soporte para recibir JSON en las solicitudes
-app.use(cors()); // Habilita CORS para permitir solicitudes desde otros dominios
+
+app.use(express.static('frontend/public'));
+
+// app.use(cors()); // Habilita CORS para permitir solicitudes desde otros dominios
+app.use(cors({
+    origin: '*', // Permitir todas las conexiones (solo para pruebas)
+    // origin: 'http://localhost:3000', //Limita la conexián a solo esta
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 
 // 📌 Rutas
 app.use('/user', usersRoutes); // Asigna las rutas de usuario al prefijo '/user'
 
-// 📌 Configuración del puerto
+// // 📌 Configuración del puerto
 const PORT = process.env.PORT || 3000; // Usa el puerto de las variables de entorno o el 3000 por defecto
+
+// Pendiente por eliminar
+app.use((req, res, next) => {
+    console.log(`Solicitud recibida: ${req.method} ${req.url}`);
+    next();
+});
 
 // 📌 Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`); // Mensaje de confirmación
+    console.log(`Servidor corriendo en http://localhostttt:${PORT}`); // Mensaje de confirmación
 });
